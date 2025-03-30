cc0876974855d3593a9915cfbf862d3f05a1ca63
Josue Morales <thejosuescript@gmail.com>
2025-03-30 13:47:10 -0600

    moving aout ruters

---
+++ server/routers/Auth.router.js
@@ -0,0 +1,235 @@
+import express from "express";
+import { AuthError } from "../errorsHandler/AuthError.class.js";
+import { UserServices } from "../services/user.services.js";
+import jwt from "jsonwebtoken";
+import { authService } from "../services/auth.services.js";
+import { Icons } from "../tools/IconGenerator.js";
+import { hasTokenExpired } from "../tools/tokenExpired.js";
+import refresher from "../tools/refreshTokens.js";
+import { authTokenMdwr } from "../midlewares/apiAuth.mdwr.js";
+import prisma from "../../prisma/postgresClient.js";
+import { compare } from "bcrypt";
+import AuthValidations from "../validations/auth.validations.service.js";
+import {errors} from "celebrate"
+const { JsonWebTokenError, sign, verify } = jwt
+const AuthRouter = express.Router();
+AuthRouter.post('/', async (req, res) => {
+    res.json(await prisma.auth.findMany())
+})
+AuthRouter.post("/api/auth/signup", AuthValidations.signupValidation ,async (req, res, next) => {
+    try {
+        const { email, password, name } = req.body;
+
+        // Validación de los datos de entrada
+        if (!email || !password || !name) {
+            throw new AuthError({
+                name: "MissingData",
+                message: "Email, password, and name are required",
+                type: "InvalidData",
+                code: 5,
+            });
+        }
+        // Verificar si el usuario ya existe
+        const existingUser = await UserServices.getUserByMail(email);
+        if (existingUser) {
+            throw new AuthError({
+                name: "UserExists",
+                message: "User with this email already exists",
+                type: "InvalidData",
+                code: 6,
+            });
+        }
+        const user = await UserServices.createUser({ email, password, name });
+        const accesToken = sign({ ID: user.id, type: 'access' }, process.env.SECRET_KEY, { expiresIn: '2d' })
+        const refreshToken = sign({ ID: user.id, type: 'refresh' }, process.env.SECRET_KEY, { expiresIn: '4d' })
+        // Generar los tokens
+        const tokens = {
+            accesToken: accesToken,
+            refreshToken: refreshToken,
+        };
+        authService.setUserTokens(user.id, tokens)
+        res.status(201).json({
+            user: {
+                name: user.name,
+                icon: Icons.genIcons(user.iconStyle, user.icon),
+                id: user.id,
+                subscriptionType: user.subscriptionType,
+                inSession: user.inSession,
+                isPremium: user.isPremium,
+            },
+            tokens,
+        });
+    } catch (error) {
+        next(error);
+    }
+},errors)
+AuthRouter.post("/api/auth/signin", AuthValidations.signinValidation ,async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        // Validación de email y password
+        if (!email || !password) {
+            throw new AuthError({
+                name: "MissingData",
+                message: "Email & Password must be provided in the body of the request",
+                type: 'InvalidData',
+                code: 4
+            });
+        }
+        if (email.length < 4) {
+            throw new AuthError({
+                name: "MissingData",
+                message: "Email & Password are not in a valid format",
+                type: 'InvalidData',
+                code: 8
+            });
+        }
+        const auth = await authService.getAuthByMail(email);
+        if (!auth) {
+            throw new AuthError({
+                name: "InexistentUser",
+                message: "The provided email does'nt belong to an existent user",
+                type: 'InexistentMail',
+                code: 5
+            });
+        }
+        // Verificación de contraseña
+        if (!(await compare(password, auth.password))) {
+            throw new AuthError({
+                name: "InvalidCredentials",
+                message: "Invalid password",
+                type: "AuthError",
+                code: 6
+            });
+        }
+        const user = await UserServices.getUserByMail(email);
+        // Función para crear tokens y responder
+        const generateAndRespondWithTokens = (user, refreshExpired = false) => {
+            const tokens = {
+                accesToken: sign({ ID: user.id, type: 'access' }, process.env.SECRET_KEY, { expiresIn: '2d' }),
+                refreshToken: refreshExpired ? sign({ ID: user.id, type: 'refresh' }, process.env.SECRET_KEY, { expiresIn: '4d' }) : auth.refreshToken
+            };
+            authService.setUserTokens(user.id, tokens);
+            res.status(200).json({
+                user: {
+                    name: user.name,
+                    icon: Icons.genIcons(user.iconStyle, user.icon),
+                    id: user.id,
+                    subscriptionType: user.subscriptionType,
+                    inSession: user.inSession,
+                    isPremium: user.isPremium
+                },
+                tokens
+            });
+        };
+
+        // Token handling
+        if (!auth.accesToken || hasTokenExpired(auth.accesToken)) {
+            if (!auth.refreshToken || hasTokenExpired(auth.refreshToken)) {
+                // Ambos tokens han expirado, generar nuevos
+                generateAndRespondWithTokens(user, true);
+            } else {
+                // Acces token ha expirado pero refresh token es válido
+                const tokens = refresher(auth.refreshToken);
+                authService.setUserTokens(user.id, tokens);
+                res.status(200).json({
+                    user: {
+                        name: user.name,
+                        icon: Icons.genIcons(user.iconStyle, user.icon),
+                        id: user.id,
+                        subscriptionType: user.subscriptionType,
+                        inSession: user.inSession,
+                        isPremium: user.isPremium
+                    },
+                    tokens
+                });
+            }
+        } else {
+            // Token de acceso válido, devolver tokens actuales
+            res.status(200).json({
+                user: {
+                    name: user.name,
+                    icon: Icons.genIcons(user.iconStyle, user.icon),
+                    id: user.id,
+                    subscriptionType: user.subscriptionType,
+                    inSession: user.inSession,
+                    isPremium: user.isPremium
+                },
+                tokens: {
+                    accesToken: auth.accesToken,
+                    refreshToken: auth.refreshToken
+                }
+            });
+        }
+    } catch (error) {
+        next(error);
+    }
+},errors);
+AuthRouter.get("/api/auth/me", AuthValidations.authToken ,authTokenMdwr, async (req, res, next) => {
+    try {
+        const userID = req.tokenPayload.ID
+        const user = await UserServices.getUserById(userID)
+        if (!user) {
+            throw new AuthError({
+                name: "userNotFound",
+                message: "The token belongs to an user that not longer exist",
+                type: "InexistentUser",
+                code: 5
+            });
+        }
+        res.status(200).json({
+            name: user.name, icon: Icons.genIcons(user.iconStyle, user.icon), id: user.id, subscriptionType: user.subscriptionType,
+            inSession: user.inSession, isPremium: user.isPremium
+        })
+    } catch (error) {
+        next(error)
+    }
+},errors)
+AuthRouter.get("/api/auth/me/renew", AuthValidations.authToken, async (req, res, next) => {
+    try {
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
+            return next(new AuthError({
+                name: 'InvalidAuthToken',
+                message: 'The authorization token was invalid or was not provided',
+                type: 'InvalidData',
+                code: 1
+            }));
+        }
+        const token = authHeader.split(' ')[1];
+        const tokenData = verify(token, process.env.SECRET_KEY);
+        if (tokenData) {
+            const currentToken = await authService.getTokensByUser(tokenData.ID);
+            const user = await UserServices.getUserById(tokenData.ID)
+            res.json({
+                token: currentToken.accesToken,
+                user: {
+                    name: user.name,
+                    icon: Icons.genIcons(user.iconStyle, user.icon),
+                    id: user.id,
+                    subscriptionType: user.subscriptionType,
+                    inSession: user.inSession,
+                    isPremium: user.isPremium,
+                }
+            })
+        } else {
+            next(new AuthError({
+                name: 'InvalidJWT',
+                message: 'The JWT format was invalid or the token is otherwise invalid',
+                type: 'InvalidToken',
+                code: 3
+            }));
+        }
+    } catch (error) {
+        if (error instanceof JsonWebTokenError) {
+            // Manejo genérico para errores de JWT (excepto token expirado)
+            next(new AuthError({
+                name: 'InvalidJWT',
+                message: 'The JWT format was invalid or the token is otherwise invalid',
+                type: 'InvalidToken',
+                code: 3
+            }));
+        }
+        next(error)
+    }
+},errors)
+export default AuthRouter
---
+++ server/routers/User.router.js
@@ -0,0 +1,229 @@
+import express from "express";
+import { authTokenMdwr } from "../midlewares/apiAuth.mdwr.js";
+import { UserServices } from "../services/user.services.js";
+import { AuthError } from "../errorsHandler/AuthError.class.js";
+import { Storage } from "../virtualdata/virtualStorage.js";
+import uploader from "../image.tools/store.js";
+import multerErrorHandler from "../midlewares/multerErrorHandler.js";
+import { Icons } from "../tools/IconGenerator.js";
+import AuthValidations from "../validations/auth.validations.service.js";
+import UserValidations from "../validations/user.validations.service.js";
+import { errors } from "celebrate";
+const UserRouter = express.Router();
+function init({ io }) {
+    UserRouter.get("/api/icons/rawlist", AuthValidations.authToken, authTokenMdwr, async (req, res, next) => {
+        try {
+            const userID = req.tokenPayload.ID
+            const user = await UserServices.getUserById(userID)
+            if (!user) {
+                throw new AuthError({
+                    name: "userNotFound",
+                    message: "The token belongs to an user that not longer exist",
+                    type: "InexistentUser",
+                    code: 5
+                });
+            }
+            res.status(200).json(Icons.genRawList({ isPremium: user.isPremium }))
+        } catch (error) {
+            next(error)
+        }
+    },errors)
+    // TODO agregar validaciones a los valores del icono
+    UserRouter.post("/api/icons/select", AuthValidations.authToken, UserValidations.iconSelection, authTokenMdwr, async (req, res, next) => {
+        try {
+            const userID = req.tokenPayload.ID
+            const user = await UserServices.getUserById(userID)
+            if (!user) {
+                throw new AuthError({
+                    name: "userNotFound",
+                    message: "The token belongs to an user that not longer exist",
+                    type: "InexistentUser",
+                    code: 5
+                });
+            }
+            const { seed, style } = req.body
+            await UserServices.updateIcon(userID, { seed, style })
+            res.status(200).send()
+        } catch (error) {
+            next(error)
+        }
+    },errors)
+    // TODO agregar validaciones al valor name
+    UserRouter.put("/api/name", AuthValidations.authToken, UserValidations.changeName, authTokenMdwr, async (req, res, next) => {
+        try {
+            const { name } = req.body
+            if (!name) {
+                throw new AuthError({ code: 9, name: "MissingData", message: "The new name is neccesary in order to change it", type: "invalidData" })
+            }
+            const userID = req.tokenPayload.ID
+            const user = await UserServices.getUserById(userID)
+            if (!user) {
+                throw new AuthError({
+                    name: "userNotFound",
+                    message: "The token belongs to an user that not longer exist",
+                    type: "InexistentUser",
+                    code: 5
+                });
+            }
+            await UserServices.updateName(userID, { name })
+            res.status(200).send()
+        } catch (error) {
+            next(error)
+        }
+    },errors)
+    UserRouter.post("/api/image/session/:session/message/:message/content/:content", UserValidations.addContent, uploader.single('image'), (req, res, next) => {
+        try {
+            const { session, message, content } = req.params
+            const file = req.file
+            if (!file) {
+                io.emit(`imageuploader-${message}`, {
+                    status: 0,
+                    reason: {
+                        cause: 'fileNotProvided',
+                        code: 0,
+                        name: 'fileMissing'
+                    }
+                })
+            }
+            let Session = Storage.findByID(session)
+            let Message = Session.chat.getMessage(message)
+            if (Session, Message) {
+                Message.editValues(content, {
+                    name: file.filename,
+                    extension: file.filename.split('.')[1],
+                    loaded: true,
+                    saved: true
+                })
+                io.emit(`imageuploader-${content}`, {
+                    status: 1,
+                    name: file.filename
+                })
+            }
+            res.sendStatus(200)
+        } catch (error) {
+            next(error)
+        }
+    }, multerErrorHandler,errors)
+    UserRouter.get("/api/image/session/:session/message/:message/content/:content", UserValidations.addContent, (req, res, next) => {
+        try {
+            const { session, message, content } = req.params
+            let Session = Storage.findByID(session)
+            let Message = Session.chat?.getMessage(message)
+            if (Session, Message) {
+                let Content = Message.getContent(content)
+                if (!Content) {
+                    res.status(404).json({ Message: 'Content not found' })
+                }
+                if (Content.type !== 'image') {
+                    res.status(404).json({ Message: 'Content is not of image type' })
+                }
+                res.status(200).json({ name: Content.body.name })
+            } else {
+                res.status(404).json({ message: 'Session or message not found' })
+            }
+        } catch (error) {
+            next(error)
+        }
+    },errors)
+    UserRouter.post('/api/collection/session/:session/message/:message/content/:content', UserValidations.addContent, uploader.array('image'), (req, res, next) => {
+        try {
+            const { session, message, content } = req.params
+            const files = req.files;
+            if (files.length == 0) {
+                io.emit(`imageuploader-${message}`, {
+                    status: 0,
+                    reason: {
+                        cause: 'filesNotProvided',
+                        code: 0,
+                        name: 'filesMissing'
+                    }
+                })
+            }
+            let collection = files.map(f => ({
+                name: f.filename,
+                extension: f.filename.split('.')[1],
+                loaded: true,
+                saved: true
+            }))
+            let Session = Storage.findByID(session)
+            let Message = Session.chat.getMessage(message)
+            if (Session, Message) {
+                console.log(collection, files)
+                Message.editValues(content, {
+                    items: collection.map(f => new ImageItem(f)),
+                    loaded: true
+                })
+                console.log(Message.getContent(content))
+                io.emit(`imageuploader-${content}`, {
+                    status: 1,
+                    items: collection
+                })
+            }
+            res.sendStatus(200)
+        } catch (error) {
+            next(error)
+        }
+    }, multerErrorHandler,errors)
+    UserRouter.get("/api/collection/session/:session/message/:message/content/:content", UserValidations.addContent, (req, res, next) => {
+        try {
+            const { session, message, content } = req.params
+            let Session = Storage.findByID(session)
+            let Message = Session.chat?.getMessage(message)
+            if (Session, Message) {
+                let Content = Message.getContent(content)
+                if (!Content) {
+                    res.status(404).json({ Message: 'Content not found' })
+                }
+                if (Content.type !== 'image') {
+                    res.status(404).json({ Message: 'Content is not of image type' })
+                }
+                if (Content.body.items) {
+                    if (Content.body.items.length == 0) {
+                        res.status(404).json({ Message: 'Collection is empty' })
+                    }
+                    res.status(200).json({ items: Content.body.items.map(i => i.build()) })
+                }
+                else {
+                    res.status(200).json({ name: Content.body.name })
+                }
+            } else {
+                res.status(404).json({ message: 'Session or message not found' })
+            }
+        } catch (error) {
+            next(error)
+        }
+    },errors)
+    UserRouter.get("/api/image/:name", UserValidations.getImage, (req, res, next) => {
+        const { name } = req.params
+        if (!name || !name.split('.')[1]) {
+            throw new SessionError({ name: 'dataNotProvided', message: 'the required data was not provided for this endpoint', type: 'data', where: 'GettingImage' })
+        }
+        let id = name.split('.')[0]
+        let ext = name.split('.')[1]
+        let validExtensions = ['jpg', 'png', 'webp', 'jpeg']
+        if (!validExtensions.includes(ext)) {
+            throw new SessionError({ name: 'invalidExtension', message: 'this extension is not a valid extension', type: 'data' })
+        }
+        const imagepath = path.join(ROOT_DIR, 'uploads', id + "." + ext)
+        res.sendFile(imagepath, (err) => {
+            if (err) {
+                console.log('notFound', err)
+                next(err)
+            }
+        })
+    },errors)
+    let twemojiMemoryPoint;
+    UserRouter.get("/api/emojis/twemoji", (req, res, next) => {
+        try {
+            const jsonData = twemojiMemoryPoint ?? JSON.parse(fs.readFileSync('data/emojis_twemoji.json', 'utf-8'));
+            if (!twemojiMemoryPoint) {
+                twemojiMemoryPoint = jsonData
+            }
+            res.json(jsonData)
+        } catch (error) {
+            next(error)
+        }
+    })
+    return UserRouter
+}
+export default init
