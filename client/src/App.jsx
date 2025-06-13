06dde678371823b1cfcf0afed478b1a9fcc94ce1
Josue Morales <thejosuescript@gmail.com>
2025-06-13 17:23:34 -0600

    using react router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router"
 import Home from "./pages/Home"
 import Recovery from "./pages/Recovery"
 
