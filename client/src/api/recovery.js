a8a2a15c31133b8ba7b7aeae7552b167b1bb8b0b
Josue Morales <thejosuescript@gmail.com>
2025-06-13 21:58:08 -0600

    API methods

---
+++ client/src/api/recovery.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+export async function recoveryRequest(mail) {
+    try {
+        let headersList = {
+            "Content-Type": mail
+        }
+        let bodyContent = JSON.stringify({ "mail": "thejosuescript@gmail.com" });
+
+        let reqOptions = {
+            url: "https://lview-party.onrender.com/api/auth/recovery/request",
+            method: "POST",
+            headers: headersList,
+            data: bodyContent,
+        }
+
+        let response = await axios.request(reqOptions);
+        return response?.data?.token
+    } catch (error) {
+        throw error
+    }
+}
+export async function recoveryValidation(token,otp){
+    try {
+        let headersList = {
+            "Content-Type": "application/json"
+        }
+        let bodyContent = JSON.stringify({ "token": token,"code":otp });
+
+        let reqOptions = {
+            url: "https://lview-party.onrender.com/api/auth/recovery/verification",
+            method: "POST",
+            headers: headersList,
+            data: bodyContent,
+        }
+
+        let response = await axios.request(reqOptions);
+        return response?.data?.token
+    } catch (error) {
+        throw error
+    }
+}
