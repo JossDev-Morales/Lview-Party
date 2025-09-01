16d6a527a3daa7af0211a9ecf7badc19f8f2b132
Josue Morales <thejosuescript@gmail.com>
2025-08-31 21:39:58 -0600

    wrong module importatin fix [js[

--- server/socket/useMiddlewares.js
+++ server/socket/useMiddlewares.js
@@ -1,5 +1,5 @@
 import { Socket } from "socket.io"
-import { SessionError } from "../errorsHandler/SessionError.class"
+import { SessionError } from "../errorsHandler/SessionError.class.js"
 
 /**
  * 
